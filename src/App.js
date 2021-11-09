707ee93aa8e064893ffc79dc92b1411c7757cb6c
Kenny Nguyen <knguyen111601@gmail.com>
2021-11-09 10:36:39 -0800

    frontend complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+// Components
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import {Route, Routes} from "react-router-dom"
+
+// Pages
+import Home from "./pages/Home"
+import About from './pages/About'
+import Projects from "./pages/Projects"
+
+
 
 function App() {
+  const URL = "https://knguyen111601express-react-lab.herokuapp.com/"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route exact path="/projects" element={<Projects URL={URL}/>}/>
+        <Route exact path="/about" element={<About URL={URL}/>}/>
+      </Routes>
     </div>
   );
 }
